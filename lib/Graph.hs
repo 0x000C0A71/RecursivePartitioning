121d0e90d96a70f4d8f6633e27bf67414cf2ae71
Marc Meier <marc.meier@coatl.ch>
2026-07-27 08:29:30 +0200

    Add Graph documentation and empty test cases
    Signed-off-by: Marc Meier <marc.meier@coatl.ch>

--- lib/Graph.hs
+++ lib/Graph.hs
@@ -8,6 +8,9 @@ module Graph
     , mergeEdge
     , getSubgraphs
     , getEdges
+
+    , dbgShow
+    , dbgVerify
     ) where
 
 
@@ -18,12 +21,19 @@ import Data.Bifunctor
 
 import Data.Map ((!))
 
-
+-- | Graph type
+-- Directed graph type. Maps from vertex `v` to `(next, previous)`
+-- where:
+-- - `next`:     are all vertices k such that the directed edge `v` -> `k` is part of the graph
+-- - `previous`: are all vertices k such that the directed edge `k` -> `v` is part of the graph
+--
+-- So it encodes, for each vertex, what it's (successors, predecessors) are
 newtype Graph v = Graph (M.Map v (S.Set v, S.Set v))
 
 empty :: Graph v
 empty = Graph M.empty
 
+-- | `addEdge k v g` returns g with the added edge `k` -> `v`
 addEdge :: forall v . Ord v => v -> v -> Graph v -> Graph v
 addEdge from to (Graph g) = Graph $ (M.alter add_from to . M.alter add_to from) g
     where
@@ -33,7 +43,8 @@ addEdge from to (Graph g) = Graph $ (M.alter add_from to . M.alter add_to from) 
         add_to :: Maybe (S.Set v, S.Set v) -> Maybe (S.Set v, S.Set v)
         add_to = Just . first (S.insert to) . fromMaybe (S.empty, S.empty)
 
-
+-- | `removeEdge k v g` returns g with the edge `k` -> `v` removed
+-- If the edge does not exist in `g`, `g` is returned unchanged
 removeEdge :: forall v . Ord v => v -> v -> Graph v -> Graph v
 removeEdge from to (Graph g) = Graph $ (M.alter remove_from to . M.alter remove_to from) g
     where
@@ -43,6 +54,14 @@ removeEdge from to (Graph g) = Graph $ (M.alter remove_from to . M.alter remove_
         remove_to :: Maybe (S.Set v, S.Set v) -> Maybe (S.Set v, S.Set v)
         remove_to = Just . first (S.delete to) . fromMaybe (S.empty, S.empty)
 
+
+-- | `mergeEdge k v m g` returns `g` with the edge `k` -> `v` "merged" into `m`
+-- After, `v` will no longer exist and will be replaced with `m`
+-- The connection from `k` to `v` will be eliminated, as `m` is supposed to express both
+-- `k` and `v`. As such `m` inherits all predecessors from `v` minus `k`, but also all
+-- predecessors of `k`. Successors remain unchanged. The edge `k` -> `v` is deleted. If this
+-- results in `k` having no remaining successors, it is also deleted. If, however, any other
+-- vertices remain for which `k` is a predecessor, `k` is kept
 mergeEdge :: forall v . Ord v => v -> v -> v -> Graph v -> Graph v
 mergeEdge from to merged (Graph g) = Graph $ foldl (.) id mods g
     where
@@ -62,6 +81,7 @@ mergeEdge from to merged (Graph g) = Graph $ foldl (.) id mods g
                 else [M.insert from (from_succs', from_preds)])
             ++ [M.delete to, M.insert merged (to_succs, to_preds')]
 
+-- | Given a graph, this function returns all disconnected subgraphs
 getSubgraphs :: forall v . Ord v => Graph v -> [Graph v]
 getSubgraphs (Graph m) = collect $ M.keysSet m
     where
@@ -73,7 +93,7 @@ getSubgraphs (Graph m) = collect $ M.keysSet m
                 chunk = go (S.findMin remaining) S.empty
 
                 graphed = S.fold (\k -> M.insert k $ m ! k) M.empty chunk
-                
+
                 go :: v -> S.Set v -> S.Set v
                 go !curr_elem !curr_sec
                     | S.member curr_elem curr_sec = curr_sec
@@ -85,7 +105,7 @@ getSubgraphs (Graph m) = collect $ M.keysSet m
 
 
 
-
+-- | Given a graph, this function returns all edges contained in the graph
 getEdges :: forall v . Ord v => Graph v -> [(v, v)]
 getEdges (Graph graph) = do
     (from, (tos, _)) <- M.assocs graph
@@ -94,7 +114,38 @@ getEdges (Graph graph) = do
 
 
 
+-- | Debug function to print the internal structure of the graph
+--
+-- Intended for debuging, may not be exported in the future
+dbgShow :: forall v . (Ord v, Show v) => Graph v -> String
+dbgShow (Graph m) = unlines $ for_each <$> M.toList m
+    where
+        for_each :: (v, (S.Set v, S.Set v)) -> String
+        for_each (el, (to, from)) = unlines $ show el : tos ++ frs
+            where
+                tos = ("-> " ++) . show <$> S.toList to
+                frs = ("<- " ++) . show <$> S.toList from
 
+-- | Check the invariant, that for an edge `k` -> `v`,
+-- `k` must be recorded as a predecessor of `v` and
+-- `v` must be recorded as a successor of `k`
+--
+-- Function either returns `True` or an error informing
+-- about the broken invariant
+--
+-- Intended for debuging, may not be exported in the future
+dbgVerify :: forall v . (Ord v, Show v) => Graph v -> Bool
+dbgVerify (Graph m) = all verify_one $ M.toList m
+    where
+        verify_one :: (v, (S.Set v, S.Set v)) -> Bool
+        verify_one (v, (to, from)) =
+            all tofn (S.toList to) &&
+            all frfn (S.toList from)
+            where
+                tofn = mb ("to set wrong for "   ++ show v) . snd . (m M.!)
+                frfn = mb ("from set wrong for " ++ show v) . fst . (m M.!)
+
+                mb msg s = S.member v s || error msg
 
 
 
