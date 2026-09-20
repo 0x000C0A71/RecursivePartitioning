e8514d644cb781fdba12db849984278a604aae51
Marc Meier <marc.meier@coatl.ch>
2026-09-20 03:36:59 +0200

    improve timing
    Signed-off-by: Marc Meier <marc.meier@coatl.ch>

--- exe/TimeMain.hs
+++ exe/TimeMain.hs
@@ -21,19 +21,28 @@ main = do
         Nothing -> return "hlo_opt"
 
     base_env <- getEnvironment
-    [module_path', runs', csv_path'] <- getArgs
+    [module_path', runs', csv_path', fuses'] <- getArgs
 
     module_path <- makeAbsolute module_path'
+    csv_path    <- makeAbsolute csv_path'
+    fuses       <- makeAbsolute fuses'
     let runs = read runs'
-    csv_path <- makeAbsolute csv_path'
     
-    run run_hlo base_env module_path runs csv_path
+    run run_hlo base_env module_path runs csv_path fuses
 
-run :: String -> Env -> FilePath -> Int -> FilePath -> IO ()
-run run_hlo base_env module_path runs csv_path = do
-    results <- call_n_times runs call_run
-    show_info results
-    writeFile csv_path $ concatMap ((++ "\n") . show) results
+run :: String -> Env -> FilePath -> Int -> FilePath -> FilePath -> IO ()
+run run_hlo base_env module_path runs csv_path fuses = do
+    results <- call_n_times runs one
+    let (baseline, ours) = unzip results
+
+    putStrLn "Baseline:"
+    show_info baseline
+    putStrLn "Ours:"
+    show_info ours
+
+    let csv_lines = (:) "baseline,ours" $ (\(l, r) -> show l ++ ',' : show r) <$> results
+
+    writeFile csv_path $ unlines csv_lines
     putStrLn $ "Written to " ++ show csv_path
     where
         call_n_times :: forall a. Int -> IO a -> IO [a]
@@ -46,8 +55,13 @@ run run_hlo base_env module_path runs csv_path = do
                     t <- if n < top then go (n+1) else return []
                     return $ v:t
 
-        call_run :: IO Double
-        call_run = do
+        one :: IO (Double, Double)
+        one = (,) <$> call_run base_env <*> call_run environ
+            where
+                environ = ("XLA_RPOF_FORCE_FILE", fuses) : base_env
+
+        call_run :: Env -> IO Double
+        call_run environ = do
             (_, _, _, process_handle) <- createProcess_ "call_run" create_process
             waitForProcess process_handle >>= \case
                 ExitSuccess -> return ()
@@ -67,7 +81,7 @@ run run_hlo base_env module_path runs csv_path = do
                 create_process = (proc run_hlo ["--platform=CUDA", module_path])
                     { std_out = NoStream
                     , std_err = NoStream
-                    , env     = Just $ ("XLA_RPOF_RUNTIME_FILE", tmp_file) : base_env
+                    , env     = Just $ ("XLA_RPOF_RUNTIME_FILE", tmp_file) : environ
                     }
 
                 tmp_file :: FilePath
